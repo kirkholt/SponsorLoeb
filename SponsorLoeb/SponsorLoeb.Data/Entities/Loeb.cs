6145852100a6634215122f94b34fbe683def5a18
Ole Kirkholt <ole@kirkholt.com>
2017-10-29 19:12:33 +0100

    Initial Commit

---
+++ SponsorLoeb/SponsorLoeb.Data/Entities/Loeb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SponsorLoeb.Data.Entities
+{
+    public class Loeb
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public decimal LaendgePrOmgang { get; set; }
+        public bool OmgangeTaelles { get; set; }
+    }
+}
---
+++ SponsorLoeb/SponsorLoeb.Data/Entities/PostBy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SponsorLoeb.Data.Entities
+{
+    public class PostBy
+    {
+        public int Id { get; set; }
+        public string PostNr { get; set; }
+        public string Bynavn { get; set; }
+    }
+}
