f6349b9309209f61ae8720132a9f226ca6781648
Ole Kirkholt <ole@kirkholt.com>
2017-11-01 16:48:03 +0100

    Tilføj validering

--- SponsorLoeb/SponsorLoeb.Data/Entities/Loeb.cs
+++ SponsorLoeb/SponsorLoeb.Data/Entities/Loeb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SponsorLoeb.Data.Entities
@@ -7,6 +8,8 @@ namespace SponsorLoeb.Data.Entities
     public class Loeb
     {
         public int Id { get; set; }
+
+        [Required]
         public string Navn { get; set; }
         public decimal LaendgePrOmgang { get; set; }
         public bool OmgangeTaelles { get; set; }
